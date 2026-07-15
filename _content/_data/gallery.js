8ff145c0391bbeeeecea59bba23bc99ec96c1375
Luis Saturno <saturno.luis@gmail.com>
2026-07-15 21:30:14 +0200

    wip

--- _content/_data/gallery.js
+++ _content/_data/gallery.js
@@ -1,61 +1,66 @@
 export default {
-    digitalArtDescription: `
+digitalArtEs: `
+Mi fascinación por el arte digital proviene de haber crecido frente a una pantalla.
+Esta inmersión, iniciada con los televisores de los años 90, me inculcó un amor por
+los colores RGB, los gradientes luminosos y la ausencia de un negro pleno; siempre hay
+luz. 
 
-# Arte Digital{.es}
-
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut nisl nec risus
-laoreet scelerisque. Sed euismod, nisl sit amet convallis consequat, turpis ligula
-luctus elit, at dictum est erat sed mauris. Nam faucibus, justo vitae consequat
-gravida, metus lacus tincidunt arcu, sed pretium lorem odio id nunc. Integer nec
-sapien vel sem bibendum dictum. Duis vel ligula ut libero tristique dictum at a
-erat.
-
-## Digital Art{.en}
-
-Dolor sit amet consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-labore et dolore magna aliqua. Viverra suspendisse potenti nullam ac tortor vitae
-purus. Tincidunt praesent semper feugiat nibh sed pulvinar proin. Nibh tortor id
-aliquet lectus proin nibh nisl condimentum. Amet consectetur adipiscing elit duis
-tristique sollicitudin nibh sit.
+Lo digital me permite experimentar con libertad, planificar bocetos con agilidad y
+llevar mis ideas a la animación. Además de su versatilidad técnica, su naturaleza
+inmediata me facilita un feedback directo y constante de quienes encuentran mis obras
+en redes sociales.
 `,
 
-    paintingsDescription: `
+digitalArtEn: `
+My fascination with digital art stems from growing up in front of a screen. This
+immersion, which began with 1990s televisions, instilled in me a love for RGB colors,
+luminous gradients, and the absence of pure black; there is always light.
 
-## Pinturas{.es}
-
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut nisl nec risus
-laoreet scelerisque. Sed euismod, nisl sit amet convallis consequat, turpis ligula
-luctus elit, at dictum est erat sed mauris. Nam faucibus, justo vitae consequat
-gravida, metus lacus tincidunt arcu, sed pretium lorem odio id nunc. Integer nec
-sapien vel sem bibendum dictum. Duis vel ligula ut libero tristique dictum at a
-erat.
-
-## Paintings{.en}
-
-Dolor sit amet consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-labore et dolore magna aliqua. Viverra suspendisse potenti nullam ac tortor vitae
-purus. Tincidunt praesent semper feugiat nibh sed pulvinar proin. Nibh tortor id
-aliquet lectus proin nibh nisl condimentum. Amet consectetur adipiscing elit duis
-tristique sollicitudin nibh sit.
+The digital medium allows me to experiment freely, plan sketches quickly, and bring my
+ideas into animation. Beyond its technical versatility, its immediate nature provides
+me with direct and constant feedback from those who find my work on social media.
 `,
 
-    drawingsDescription: `
+paintingsEs: `
+El trabajo con óleo sobre lienzo o papel (generalmente en formatos medianos a
+pequeños) es mi cable a tierra. 
 
-# Dibujos{.es}
+Pintar en este medio tan tradicional es una forma de honrar y mantener vigente el
+oficio de la pintura, desarrollando la paciencia y recordando que no todo debe ser
+inmediato. Esta práctica me resulta fundamental para lograr un equilibrio consciente
+con la tecnología, porque ofrece un contrapunto tangible y duradero a la fugacidad de
+la pantalla.
+`,
 
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut nisl nec risus
-laoreet scelerisque. Sed euismod, nisl sit amet convallis consequat, turpis ligula
-luctus elit, at dictum est erat sed mauris. Nam faucibus, justo vitae consequat
-gravida, metus lacus tincidunt arcu, sed pretium lorem odio id nunc. Integer nec
-sapien vel sem bibendum dictum. Duis vel ligula ut libero tristique dictum at a
-erat.
+paintingsEs: `
+Working with oil on canvas or paper (generally in small to medium formats) is my
+grounding wire.
 
-## Drawings{.en}
+Painting in this highly traditional medium is a way to honor and preserve the craft of
+painting, developing patience and serving as a reminder that not everything has to be
+immediate. This practice is essential for me to achieve a conscious balance with
+technology, as it offers a tangible and lasting counterpoint to the temporary nature
+of the screen.
+`,
 
-Dolor sit amet consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-labore et dolore magna aliqua. Viverra suspendisse potenti nullam ac tortor vitae
-purus. Tincidunt praesent semper feugiat nibh sed pulvinar proin. Nibh tortor id
-aliquet lectus proin nibh nisl condimentum. Amet consectetur adipiscing elit duis
-tristique sollicitudin nibh sit.
+drawingsEs: `
+El dibujo es, quizás, mi técnica más íntima. Me permite abstraerme por horas, y el
+proceso con el grafito y el carbón se siente muy cercano a la meditación. 
+
+Es una técnica noble que ofrece el placer de descubrir formas y volúmenes a través de
+la mancha, con la certeza de que siempre puedo borrar y volver a comenzar. Me atrae
+profundamente el dramatismo que se puede lograr con el claroscuro; utilizo la luz y la
+sombra para dotar de una intensidad emocional única a las figuras que emergen del
+papel.
+`,
+
+drawingsEn: `
+Drawing is, perhaps, my most intimate practice. It allows me to tune out the world for
+hours, and working with graphite and charcoal feels a lot like meditation.
+
+It’s a generous medium that offers the pleasure of discovering shapes and volume
+through shading, knowing that I can always erase and start over. I’m deeply drawn to
+the drama you can achieve with chiaroscuro; I use light and shadow to give a unique
+emotional intensity to the figures that emerge from the paper.
 `,
 };
